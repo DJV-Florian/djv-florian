010198651f4a1677fe68c0bc98d0dd5720dfc3a8
DJV-Florian <165883107+DJV-Florian@users.noreply.github.com>
2024-10-02 14:31:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,3 +79,19 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add event listener to the theme toggle button
     document.getElementById('theme-toggle-btn').addEventListener('click', toggleTheme);
 });
+
+function adjustViewportForSmartphone() {
+    function isSmartphone() {
+        return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    }
+
+    if (isSmartphone()) {
+        const viewport = document.querySelector("meta[name='viewport']");
+        
+        if (viewport) {
+            viewport.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", adjustViewportForSmartphone);
